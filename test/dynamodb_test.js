34809df22b27d997f6217ad37cdbe1a7ec8176ea
Chris Dellinger <cdellinger@gmail.com>
2015-12-27 21:34:36 -0500

    refactor: refactored to allow for different providers and implemented dynamodb provider

--- test/dynamodb_test.js
+++ test/dynamodb_test.js
@@ -1,39 +1,24 @@
 var assert = require('assert');
 var should = require('should');
 
-var host = process.env.TEST_DOCUMENTDB_HOST_URL;
-var masterKey = process.env.TEST_DOCUMENTDB_MASTER_KEY;
-var collectionUrl = process.env.TEST_DOCUMENTDB_COLLECTION_URL;
-
-
-//test data
-var UserSchema = require('../lib/schema2');
+var UserSchema = require('../lib/user_schema');
+var dynamodb_provider = require('../lib/dynamodb_provider');
 
 
 // Test Data
+var testTenant = '#TEST_TENANT#';
+var user1 = {userHandle: '|||TESTUSER1|||', twitterHandle: '###TESTUSER1_TWITTER!###'};
+var user2 = {userHandle: '|||TESTUSER2|||', twitterHandle: '###TESTUSER2_TWITTER!###'};
+var pocketUser = {userHandle: '|||POCKET_TESTUSER1|||', token: 'TEST_TOKEN', token2: 'TEST_TOKEN2'}; 
+
 var tenantUserId = '';		// will be populated later in the tests
 var tenantlessUserId = '';  // will be populated later in the tests
 
-var testTenant = '#TEST_TENANT#';
-var testUser1UserHandle = '|||TESTUSER1|||';
-var testUser1TwitterID = '###TESTUSER1_TWITTER!###';
-
-var testUser2UserHandle = '|||TESTUSER2|||';
-var testUser2TwitterID = '###TESTUSER2_TWITTER!###';
-
-
-
-var passwordTenantUser = new UserSchema();
+var passwordTenantUser = new UserSchema(dynamodb_provider);
 var passwordTenantUserId = '';
 var passwordTenantUserPassword = '###PASSWORD1###'; 
 passwordTenantUser.userHandle = '|||TESTUSER3|||';
 passwordTenantUser.tenant = testTenant;
-//passwordTenantUser.addLocalStrategy(passwordTenantUser.userHandle, passwordTenantUserPassword, function(err, results){
-//	console.log('------');
-//	console.log(err);
-//	console.log(results);
-//	console.log('------');
-//});
 
 //end of test data
 
@@ -50,7 +35,6 @@ describe('Local Strategy Tests', function() {
 
 			passwordTenantUser.addLocalStrategy(passwordTenantUser.userHandle, passwordTenantUserPassword, function(err, results){
 				if (err) throw err;
-
 				passwordTenantUser.save(function(err, results){
 					if (err) throw err;
 					passwordTenantUserId = passwordTenantUser.id;
@@ -64,7 +48,7 @@ describe('Local Strategy Tests', function() {
 
 	describe('Add local strategy to user that already has a local strategy', function () {
 		it('should fail to add duplicate strategy', function (done) {
-			var user = new UserSchema();
+			var user = new UserSchema(dynamodb_provider);
 			user.loginViaPassword(passwordTenantUser.userHandle, passwordTenantUserPassword, passwordTenantUser.tenant, function(err, results){
 				if (err) throw err;
 				user.id.should.not.equal('');
@@ -75,10 +59,9 @@ describe('Local Strategy Tests', function() {
 			});
 		});
 	});
-
 	describe('Password Login Tenant User', function () {
 		it('should login successfully', function (done) {
-			var testUser = new UserSchema();
+			var testUser = new UserSchema(dynamodb_provider);
 			testUser.loginViaPassword(passwordTenantUser.userHandle, passwordTenantUserPassword, passwordTenantUser.tenant, function(err, results){
 				if (err) throw err;
 				testUser.userHandle.should.equal(passwordTenantUser.userHandle);
@@ -90,7 +73,7 @@ describe('Local Strategy Tests', function() {
 	});
 	describe('Failed Password Login Tenant User', function () {
 		it('should not login', function (done) {
-			var testUser = new UserSchema();
+			var testUser = new UserSchema(dynamodb_provider);
 			testUser.loginViaPassword(passwordTenantUser.userHandle, passwordTenantUserPassword + 'XXX', passwordTenantUser.tenant, function(err, results){
 				if (err) throw err;
 				results.should.equal(false);
@@ -102,7 +85,7 @@ describe('Local Strategy Tests', function() {
 	});
 	describe('Failed Login, Valid Password, Invalid  Login Tenant User', function () {
 		it('should not login', function (done) {
-			var testUser = new UserSchema();
+			var testUser = new UserSchema(dynamodb_provider);
 			testUser.loginViaPassword(passwordTenantUser.userHandle, passwordTenantUserPassword, '', function(err, results){
 				if (err) throw err;
 				results.should.equal(false);
@@ -115,8 +98,6 @@ describe('Local Strategy Tests', function() {
 });
 
 
-
-/*
 describe('Pocket Strategy Tests', function() {
 	this.timeout(5000);
 	before(function(){
@@ -124,14 +105,15 @@ describe('Pocket Strategy Tests', function() {
 	});
 	describe('Create user without tenant with Pocket strategy', function () {
 		it('should create user', function (done) {
-			var usr = new UserSchema();
+			var usr = new UserSchema(dynamodb_provider);
 
-			usr.userHandle = '|||POCKET_TESTUSER1|||';
-			usr.addPocketStrategy(usr.userHandle, 'TEST_TOKEN', function(err, results){
+			usr.userHandle = pocketUser.userHandle;
+			usr.addPocketStrategy(usr.userHandle, pocketUser.token, function(err, results){
 				if (err) throw err;
 				usr.save(function(err, data){
 					if (err) throw err;
 					usr.id.should.not.equal('');
+					usr.userHandle.should.equal(pocketUser.userHandle);
 					done();
 				});
 			});
@@ -140,16 +122,15 @@ describe('Pocket Strategy Tests', function() {
 
 	describe('Login user without tenant with Pocket strategy', function () {
 		it('should find user and update token', function (done) {
-			var usr = new UserSchema();
-			var newToken = 'TEST_TOKEN2';
-			usr.loginViaPocket('|||POCKET_TESTUSER1|||', newToken, '', function(err, results){
+			var usr = new UserSchema(dynamodb_provider);
+			usr.loginViaPocket(pocketUser.userHandle, pocketUser.token2, '', function(err, results){
 				if (err) throw err;
 				usr.id.should.not.equal('');
-				usr.userHandle.should.equal('|||POCKET_TESTUSER1|||');
+				usr.userHandle.should.equal(pocketUser.userHandle);
 				var foundPocketStrategy = false;
 				for (var x=0;x<usr.strategies.length;x++){
 					if (usr.strategies[x].type === 'POCKET'){
-						usr.strategies[x].token.should.equal(newToken);
+						usr.strategies[x].token.should.equal(pocketUser.token2);
 						foundPocketStrategy = true;
 					}
 				}
@@ -161,25 +142,26 @@ describe('Pocket Strategy Tests', function() {
 
 	describe('Remove pocket user without tenant', function () {
 		it('should remove matching user', function (done) {
-			var usr = new UserSchema();
-			usr.loginViaPocket('|||POCKET_TESTUSER1|||', 'TEST_TOKEN2', '', function(err, results){
+			var usr = new UserSchema(dynamodb_provider);
+
+			usr.loginViaPocket(pocketUser.userHandle, pocketUser.token2, '', function(err, results){
 				if (err) throw err;
 				usr.id.should.not.equal('');
-				usr.userHandle.should.equal('|||POCKET_TESTUSER1|||');
+				usr.userHandle.should.equal(pocketUser.userHandle);
 
 				usr.remove(function(err2, results2){
 					if (err2) throw err2;
-					usr.loginViaPocket('|||POCKET_TESTUSER1|||', 'TEST_TOKEN2', '', function(err3, results3){
+					usr.loginViaPocket(pocketUser.userHandle, pocketUser.token2, '', function(err3, results3){
 						if (err3) throw err3;
 						usr.id.should.equal('');
+						done();
 					});
 				});
-				done();
 			});
 		});
 	});
 });
-*/
+
 
 describe('Cross Strategy Tests', function() {
 	this.timeout(5000);
@@ -189,7 +171,7 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Create user with multiple strategies', function(){
 		it('should not allow the creation of the second strategy', function(done){
-			var user = new UserSchema();
+			var user = new UserSchema(dynamodb_provider);
 			user.addPocketStrategy('TESTUSER', 'TESTHANDLE', function(err, results){
 				if (err) throw err;
 				user.addLocalStrategy('TESTUSER', 'PASSWORD', function(err2, results2){
@@ -200,15 +182,13 @@ describe('Cross Strategy Tests', function() {
 		});
 	});
 
-
-
 	describe('Save With Tenant', function () {
 		it('should persist without error', function (done) {
-			var tenantUser = new UserSchema();
-			tenantUser.userHandle = testUser1UserHandle;
+			var tenantUser = new UserSchema(dynamodb_provider);
+			tenantUser.userHandle = user1.userHandle;
 			tenantUser.tenant = testTenant;
 			tenantUser.location = 'Edinburg, VA';
-			tenantUser.addTwitterStrategy(testUser1TwitterID, 'TWITTER_TOKEN', function(err, results){
+			tenantUser.addTwitterStrategy(user1.twitterHandle, 'TWITTER_TOKEN', function(err, results){
 				if (err) throw err;
 				tenantUser.save(function(err, results){
 					if (err) throw err;
@@ -222,9 +202,9 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Save Without Tenant', function () {
 		it('should persist without error', function (done) {
-			var tenantlessUser = new UserSchema();
-			tenantlessUser.userHandle = testUser2UserHandle;
-			tenantlessUser.addTwitterStrategy(testUser2TwitterID, 'TWITTER_TOKEN', function(err, results){
+			var tenantlessUser = new UserSchema(dynamodb_provider);
+			tenantlessUser.userHandle = user2.userHandle;
+			tenantlessUser.addTwitterStrategy(user2.twitterHandle, 'TWITTER_TOKEN', function(err, results){
 				if (err) throw err;
 				tenantlessUser.save(function(err, results){
 					if (err) throw err;
@@ -237,10 +217,10 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Get By Strategy With Tenant', function () {
 		it('should retrieve matching user', function (done) {
-			var testUser = new UserSchema();	
-			testUser.getByStrategy('TWITTER', testUser1TwitterID, testTenant, function(err, user){
+			var testUser = new UserSchema(dynamodb_provider);	
+			testUser.getByStrategy('TWITTER', user1.twitterHandle, testTenant, function(err, user){
 				if (err) throw err;
-				testUser.id.should.equal(testUser1TwitterID);
+				testUser.id.should.equal(user1.twitterHandle);
 				done();
 			});
 		});
@@ -248,10 +228,10 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Get By Strategy Without Tenant', function () {
 		it('should retrieve matching user', function (done) {
-			var testUser = new UserSchema();	
-			testUser.getByStrategy('TWITTER', testUser2TwitterID, '', function(err, result){
+			var testUser = new UserSchema(dynamodb_provider);	
+			testUser.getByStrategy('TWITTER', user2.twitterHandle, '', function(err, result){
 				if (err) throw err;
-				testUser.id.should.equal(testUser2TwitterID);
+				testUser.id.should.equal(user2.twitterHandle);
 				done();
 			});
 		});
@@ -259,7 +239,7 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Get By Strategy With Tenant with Invalid Strategy Id', function () {
 		it('should retrieve matching user', function (done) {
-			var testUser = new UserSchema();			
+			var testUser = new UserSchema(dynamodb_provider);			
 			testUser.getByStrategy('TWITTER', 'INVALID_USER_STRATEGY_ID', testTenant, function(err, user){
 				if (err) throw err;
 				testUser.id.should.equal('');
@@ -270,7 +250,7 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Get By Strategy Without Tenant with Invalid Strategy Id', function () {
 		it('should retrieve matching user', function (done) {
-			var testUser = new UserSchema();			
+			var testUser = new UserSchema(dynamodb_provider);			
 			testUser.getByStrategy('TWITTER', 'INVALID_USER_STRATEGY_ID', '', function(err, user){
 				if (err) throw err;
 				testUser.id.should.equal('');
@@ -281,8 +261,8 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Get By Strategy With Invalid Tenant', function () {
 		it('should retrieve undefined value', function (done) {
-			var testUser = new UserSchema();			
-			testUser.getByStrategy('TWITTER', testUser1TwitterID, 'INVALID_TENANT', function(err, user){
+			var testUser = new UserSchema(dynamodb_provider);			
+			testUser.getByStrategy('TWITTER', user1.twitterHandle, 'INVALID_TENANT', function(err, user){
 				if (err) throw err;
 				testUser.id.should.equal('');
 				done();	
@@ -293,7 +273,7 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Update User with Tenant', function () {
 		it('should update successfully', function (done) {
-			var testUser = new UserSchema();
+			var testUser = new UserSchema(dynamodb_provider);
 			testUser.getById(tenantUserId, function(err, results){
 				if (err) throw err;
 				testUser.id.should.not.equal('');
@@ -304,7 +284,7 @@ describe('Cross Strategy Tests', function() {
 
 				testUser.save(function(err2, results2){
 					if (err2) throw err2;
-					var testUser2 = new UserSchema();
+					var testUser2 = new UserSchema(dynamodb_provider);
 					testUser2.getById(tenantUserId, function(err3, results3){
 						if (err3) throw err3;
 						testUser2.email.should.equal(newEmail);
@@ -318,7 +298,7 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Update User with Tenant on optional fields', function () {
 		it('should update successfully', function (done) {
-			var testUser = new UserSchema();
+			var testUser = new UserSchema(dynamodb_provider);
 			testUser.getById(tenantUserId, function(err, results){
 				if (err) throw err;
 				testUser.id.should.not.equal('');
@@ -330,7 +310,7 @@ describe('Cross Strategy Tests', function() {
 				testUser.avatar = 'http://www.avatar.com/me';
 				testUser.save(function(err2, results2){
 					if (err2) throw err2;
-					var testUser2 = new UserSchema();
+					var testUser2 = new UserSchema(dynamodb_provider);
 					testUser2.getById(tenantUserId, function(err3, results3){
 						if (err3) throw err3;
 						testUser2.location.should.equal('Boulder, CO');
@@ -346,7 +326,7 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Update User with Tenant by nulling optional fields', function () {
 		it('should update successfully', function (done) {
-			var testUser = new UserSchema();
+			var testUser = new UserSchema(dynamodb_provider);
 			testUser.getById(tenantUserId, function(err, results){
 				if (err) throw err;
 				testUser.id.should.not.equal('');
@@ -358,7 +338,7 @@ describe('Cross Strategy Tests', function() {
 				testUser.avatar = '';
 				testUser.save(function(err2, results2){
 					if (err2) throw err2;
-					var testUser2 = new UserSchema();
+					var testUser2 = new UserSchema(dynamodb_provider);
 					testUser2.getById(tenantUserId, function(err3, results3){
 						if (err3) throw err3;
 						testUser2.location.should.equal('');
@@ -375,14 +355,14 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Update User without Tenant', function () {
 		it('should update successfully', function (done) {
-			var testUser = new UserSchema();			
+			var testUser = new UserSchema(dynamodb_provider);			
 			testUser.getById(tenantlessUserId, function(err, results){
 				if (err) throw err;
 				var newEmail = testUser.email + '1234';
 				testUser.email = newEmail;
 				testUser.save(function(err2, data){
 					if (err2) throw err2;
-					var testUser2 = new UserSchema();
+					var testUser2 = new UserSchema(dynamodb_provider);
 					testUser2.getById(tenantlessUserId, function(err3, results){
 						if (err3) throw err3;
 						testUser2.email.should.equal(newEmail);
@@ -395,10 +375,10 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Create duplicate user with tenant', function () {
 		it('should fail to create duplicate user', function (done) {
-			var tenantUser = new UserSchema();
-			tenantUser.userHandle = testUser1UserHandle;
+			var tenantUser = new UserSchema(dynamodb_provider);
+			tenantUser.userHandle = user1.userHandle;
 			tenantUser.tenant = testTenant;
-			tenantUser.addTwitterStrategy(testUser1TwitterID + 'abcd', 'TWITTER_TOKEN', function(err, results){
+			tenantUser.addTwitterStrategy(user1.twitterHandle + 'abcd', 'TWITTER_TOKEN', function(err, results){
 				if (err) throw err;
 				tenantUser.save(function(errSave, data){
 					errSave.message.should.equal('User exists already with this user handle');
@@ -410,11 +390,11 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Create duplicate user without tenant', function () {
 		it('should fail to create duplicate user', function (done) {
-			var tenantlessUser = new UserSchema();
-			var tenantlessUserId = testUser2UserHandle;
+			var tenantlessUser = new UserSchema(dynamodb_provider);
+			var tenantlessUserId = user2.userHandle;
 
-			tenantlessUser.userHandle = testUser2UserHandle;
-			tenantlessUser.addTwitterStrategy(testUser2TwitterID + 'abcd', 'TWITTER_TOKEN', function(err, results){
+			tenantlessUser.userHandle = user2.userHandle;
+			tenantlessUser.addTwitterStrategy(user2.twitterHandle + 'abcd', 'TWITTER_TOKEN', function(err, results){
 				if (err) throw err;
 				tenantlessUser.save(function(errSave, data){
 					errSave.message.should.equal('User exists already with this user handle');
@@ -427,10 +407,10 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Create user with tenant with duplicated strategy', function () {
 		it('should fail to create user', function (done) {
-			var usr = new UserSchema();
+			var usr = new UserSchema(dynamodb_provider);
 			usr.userHandle = '|||TESTUSER3|||';
 			usr.tenant = testTenant;
-			usr.addTwitterStrategy(testUser1TwitterID, 'TWITTER_TOKEN', function(err, results){
+			usr.addTwitterStrategy(user1.twitterHandle, 'TWITTER_TOKEN', function(err, results){
 				err.message.should.equal('This strategy is already in use');
 				done();
 			});
@@ -439,9 +419,9 @@ describe('Cross Strategy Tests', function() {
 
 	describe('Create user without tenant with duplicated strategy', function () {
 		it('should fail to create user', function (done) {
-			var usr = new UserSchema();
+			var usr = new UserSchema(dynamodb_provider);
 			usr.userHandle = '|||TESTUSER3|||';
-			usr.addTwitterStrategy(testUser2TwitterID, 'TWITTER_TOKEN', function(err, results){
+			usr.addTwitterStrategy(user2.twitterHandle, 'TWITTER_TOKEN', function(err, results){
 				err.message.should.equal('This strategy is already in use');
 				done();
 			});
@@ -460,7 +440,7 @@ describe('Cleanup Tests', function() {
 
 	describe('Remove user with tenant', function () {
 		it('should remove matching user', function (done) {
-			var testUser = new UserSchema();
+			var testUser = new UserSchema(dynamodb_provider);
 			testUser.getById(tenantUserId, function(err, results){
 				if (err) throw err;
 				testUser.remove(function(err2, data){
@@ -477,7 +457,7 @@ describe('Cleanup Tests', function() {
 
 	describe('Remove user without tenant', function () {
 		it('should remove matching user', function (done) {
-			var testUser = new UserSchema();
+			var testUser = new UserSchema(dynamodb_provider);
 			testUser.getById(tenantlessUserId, function(err, results){
 				if (err) throw err;
 				testUser.remove(function(err2, data){
@@ -494,7 +474,7 @@ describe('Cleanup Tests', function() {
 
 	describe('Remove password tenant user', function () {
 		it('should remove matching user', function (done) {
-			var testUser = new UserSchema();			
+			var testUser = new UserSchema(dynamodb_provider);			
 			testUser.getById(passwordTenantUserId, function(err, results){
 				if (err) throw err;
 				testUser.remove(function(err2, data){
@@ -508,23 +488,6 @@ describe('Cleanup Tests', function() {
 			});
 		});
 	});
-
-	/*
-	describe('Create user with invalid schema (no docType)', function(){
-		it('should not save successfully', function(done){
-			var tenantlessUser = new UserSchema();
-			tenantlessUser.userHandle = testUser2UserHandle;
-			tenantlessUser.addTwitterStrategy(testUser2TwitterID, 'TWITTER_TOKEN', function(err, results){
-				if (err) throw err;
-				tenantlessUser.docType = undefined;
-				tenantlessUser.save(function(err, data){
-					err.message.should.equal('docType property has been removed, it is required');
-					done();
-				});
-			});
-		});
-	});
-	*/
 	
 	after(function(){
 	});

6860a8a8eb0b8fd0836c3ac30e27a150250e78d4
Chris Dellinger <cdellinger@gmail.com>
2015-12-31 07:15:31 -0500

    test: adding support for common test for different providers

---
+++ test/providers_test.js
@@ -0,0 +1,10 @@
+function importTest(name, path, provider) {
+    describe(name, function () {
+        require(path)(provider);
+    });
+}
+describe("Provider Tests", function () {
+    importTest("DocumentDB", './shared/core_test.js', require('../lib/documentdb_provider'));
+    importTest("DynamoDB", './shared/core_test.js', require('../lib/dynamodb_provider'));
+    //importTest("b", './b/b');
+});

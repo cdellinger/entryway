d1505ab24700d2a88174bf329804922914223640
Chris Dellinger <cdellinger@gmail.com>
2016-01-02 14:04:45 -0500

    refactor: removing files that have been migrated to sub-projects

--- test/providers_test.js
+++
@@ -1,9 +0,0 @@
-function importTest(name, path, provider) {
-    describe(name, function () {
-        require(path)(provider);
-    });
-}
-describe("Provider Tests", function () {
-    importTest("DocumentDB", './shared/core_test.js', require('../lib/documentdb_provider'));
-    importTest("DynamoDB", './shared/core_test.js', require('../lib/dynamodb_provider'));
-});
